103f9024548249970e01a9bc46639863f9bf3bc4
Leon Huggins <lhuggins@corval.io>
2023-11-16 17:25:44 -0500

    Added: Authentication via interceptors, guards and custom param decorators.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,35 +7,69 @@ import {
 	Body,
 	Param,
 	Query,
-	NotFoundException
-}                        from '@nestjs/common';
-import { UsersService }  from './users.service';
-import { User }          from '@prisma/client';
-import { CreateUserDto } from './dtos/create-user.dto';
-import { UpdateUserDto } from './dtos/update-user.dto';
+	NotFoundException,
+	Session,
+	UseGuards
+}                                from '@nestjs/common';
+import { UsersService }          from './users.service';
+import { AuthService }           from '../auth/auth/auth.service';
+import { User }                  from '@prisma/client';
+import { CreateUserDto }         from './dtos/create-user.dto';
+import { UpdateUserDto }         from './dtos/update-user.dto';
+import { Serialize }             from '../interceptors/serialize.interceptor';
+import { UserDto }               from './dtos/user.dto';
+import { CurrentUser } from './decorators/current-user.decorator';
+import { AuthGuard }   from '../guards/auth.guard';
 
 @Controller( 'auth' )
+@Serialize( UserDto )
+
 export class UsersController {
-	constructor( private usersService: UsersService ){}
+	constructor( private usersService: UsersService, private authService: AuthService ){}
+	
+	
+	@Get('/whoami')
+	@UseGuards(AuthGuard)
+	whoAmI(@CurrentUser() user: User) {
+		return user
+	}
 	
 	@Post( '/signup' )
-	async createUser( @Body() body: CreateUserDto ): Promise<User>{
-		const { email, password, role } = body
-		return this.usersService.createUser( email, password, role )
+	async createUser( @Body() body: CreateUserDto, @Session() session: any ): Promise<User>{
+		const { email, password, role } = body;
+		const user = await this.authService.signup( email, password, role )
+		session.userid = user.id
+		
+		return user
+	}
+	
+	@Post( '/signin' )
+	async signIn( @Body() body: CreateUserDto, @Session() session: any ){
+		const { email, password } = body;
+		const user = await this.authService.signin( email, password )
+		session.userid = user.id
+		
+		return user
+	}
+	
+	@Post('/signout')
+	async signOut(@Session() session:any) {
+		console.log('Signed Out')
+		session.userid = null;
 	}
 	
 	@Get( '/users' )
-	async findAll(): Promise<User[]>{
-		return this.usersService.findAll()
+	async findAll(): Promise<Pick<User, 'id'>[]>{
+		return this.usersService.findAll();
 	}
 	
 	@Get( 'email' )
 	async findByEmail( @Query( 'email' ) email: string ){
-		const user = await this.usersService.findByEmail( email )
+		const user = await this.usersService.findByEmail( email );
 		if( !user ){
 			throw new NotFoundException( `User with email ${ email } not found` );
 		}
-		return user
+		return user;
 	}
 	
 	@Get( '/:id' )
@@ -47,14 +81,16 @@ export class UsersController {
 		return user;
 	}
 	
-	@Patch('/updateuser')
+	@Patch( '/update-user' )
 	async updateUser( @Query( 'email' ) email: string, @Body() body: UpdateUserDto ){
-		
-		console.log(body)
-		const { newEmail, newRole } = body
-		return this.usersService.updateUserByEmail( email, newEmail, newRole )
-		
-		
+		console.log( body );
+		const { newEmail, newRole } = body;
+		return this.usersService.updateByEmail( email, newEmail, newRole );
 	}
 	
+	@Delete( '/delete-user' )
+	async deleteUser( @Query( 'email' ) email: string ){
+		console.log( 'User Deleted' );
+		return this.usersService.deleteByEmail( email );
+	}
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,22 @@
-import { Module }          from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService }    from './users.service';
-import { PrismaService}    from '../prisma.service';
+import { Module }                 from '@nestjs/common';
+import { APP_INTERCEPTOR }        from '@nestjs/core';
+import { UsersController }        from './users.controller';
+import { UsersService }           from './users.service';
+import { PrismaService }          from '../prisma.service';
+import { AuthService }            from '../auth/auth/auth.service';
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 
-@Module({
-  controllers: [UsersController],
-  providers: [UsersService, PrismaService]
-})
+@Module( {
+	controllers: [ UsersController ],
+	providers:   [
+		UsersService,
+		PrismaService,
+		AuthService,
+		{
+			provide:  APP_INTERCEPTOR,
+			useClass: CurrentUserInterceptor
+		}
+	
+	]
+} )
 export class UsersModule {}
