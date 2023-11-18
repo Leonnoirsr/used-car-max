f1b1eea2c7e1ac5227e6d3e30d4144bb80ad3117
Leon Huggins <lhuggins@corval.io>
2023-11-17 20:37:01 -0500

    Added: Modal and Navbar components.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,18 +1,23 @@
-import { NestFactory }   from '@nestjs/core';
-import { ValidationPipe} from '@nestjs/common';
-import { AppModule }     from './app.module';
-const cookieSession = require('cookie-session');
+import { NestFactory }    from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule }      from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cookieSession({
-      keys: ['oatmeal']
-  }))
-  app.useGlobalPipes(
-      new ValidationPipe({
-        whitelist: true
-      })
-  )
-  await app.listen(3000);
+const cookieSession = require( 'cookie-session' );
+
+async function bootstrap(){
+	const app = await NestFactory.create( AppModule );
+	app.use( cookieSession( {
+		keys: [ 'oatmeal' ]
+	} ) )
+	app.useGlobalPipes(
+		new ValidationPipe( {
+			whitelist: true
+		} )
+	)
+	
+	app.enableCors();
+	app.setGlobalPrefix( 'api' );
+	await app.listen( 3001 );
 }
+
 bootstrap();
