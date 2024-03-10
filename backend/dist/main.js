066fd81f281e2ac6d26760578d065ee3c05e4c46
Leon Huggins <lhuggins@corval.io>
2024-03-10 12:22:59 -0400

    Update: Backend. to use pre-vercel configuration. Added: necessary npm packages to package.json.

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -3,24 +3,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const common_1 = require("@nestjs/common");
 const app_module_1 = require("./app.module");
-const platform_express_1 = require("@nestjs/platform-express");
-const express = require("express");
 const cookieSession = require('cookie-session');
 async function bootstrap() {
-    const server = express();
-    const app = await core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(server));
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.use(cookieSession({
-        keys: ['oatmeal']
+        keys: ['oatmeal'],
     }));
     app.useGlobalPipes(new common_1.ValidationPipe({
-        whitelist: true
+        whitelist: true,
     }));
-    app.enableCors({
-        origin: 'https://used-car-max.vercel.app/',
-        credentials: true,
-    });
+    app.enableCors();
     app.setGlobalPrefix('api');
-    await app.init();
+    await app.listen(3001);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
